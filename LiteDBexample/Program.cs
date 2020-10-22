3c90f226b67f5fd46a2bbd901d7de450d0421424
user <user@LPC2AUTOITPROG>
2020-10-22 10:38:04 +0600

    first working example

--- LiteDBexample/Program.cs
+++ LiteDBexample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LiteDB;
 
 namespace LiteDBexample
 {
@@ -10,6 +11,53 @@ namespace LiteDBexample
     {
         static void Main(string[] args)
         {
+            using(var db = new LiteDatabase(@"MyBasa.db"))
+            {
+                var col = db.GetCollection<Company>("companies");
+
+                var microsoft = new Company { Name = "Microsoft" };
+                microsoft.Users = new List<User> { new User { Name = "Bill Gates" } };
+                col.Insert(microsoft);
+
+                microsoft.Name = "Microsoft Inc.";
+                col.Update(microsoft);
+
+                var google = new Company { Name = "Gogle" };
+                google.Users = new List<User> { new User { Name = "Larry Page" } };
+                col.Insert(google);
+
+                var result = col.FindAll();
+                foreach (Company c in result)
+                {
+                    Console.WriteLine(c.Name);
+
+                    foreach (User u in c.Users)
+                    {
+                        Console.WriteLine(u.Name);
+                    }
+                    Console.WriteLine();
+                }
+
+                col.EnsureIndex(x => x.Name);
+
+               
+                col.DeleteMany(x => x.Name.Equals("Google"));
+                //Console.WriteLine("После удаления Google");
+
+                result = col.FindAll();
+                foreach(Company c in result)
+                {
+                    Console.WriteLine(c.Name);
+                    foreach (User u in c.Users)
+                    {
+                        Console.WriteLine(u.Name);
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+
+            }
         }
     }
 }
